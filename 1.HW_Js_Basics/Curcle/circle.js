330c2af105b17f8b44a7863207b11afc2ce7d69e
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 14:20:04 +0300

    Update circle.js

--- 1.HW_Js_Basics/Curcle/circle.js
+++ 1.HW_Js_Basics/Curcle/circle.js
@@ -1,4 +1,4 @@
 var radius = 2.14;
 var s = Math.PI * Math.pow(radius,2);
-console.log("Формула: S = π. r2 " + "<br>"+" Зададен радиус = " + radius + ",<br>" + " лицето на окръжността е = " + s);
-document.getElementById('space').innerHTML = ("Формула: S = π. r2 " + "<br>"+" Зададен радиус = " + radius + ",<br>" + " лицето на окръжността е = " + s);
+  console.log("Формула: S = π. r2 " + "<br>"+" Зададен радиус = " + radius + ",<br>" + " лицето на окръжността е = " + s);
+  document.getElementById('space').innerHTML = ("Формула: S = π. r2 " + "<br>"+" Зададен радиус = " + radius + ",<br>" + " лицето на окръжността е = " + s);
