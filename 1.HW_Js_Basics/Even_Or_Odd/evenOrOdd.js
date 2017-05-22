603e0c999a0f679bad2e1f1662914708df90ea0f
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 14:20:57 +0300

    Update evenOrOdd.js

--- 1.HW_Js_Basics/Even_Or_Odd/evenOrOdd.js
+++ 1.HW_Js_Basics/Even_Or_Odd/evenOrOdd.js
@@ -1,18 +1,16 @@
-  var x = 2;
+var x = 2;
   
-  if(x %2 )
-       {
+  if(x %2 ){
          text = x + " е нечетно число.";
          document.getElementById("space").innerHTML = text;
          console.log(text);
          
-       }
-  else
-    {
+    }
+  else{
       text = x + " е четно число.";
       document.getElementById("space").innerHTML = text;
       console.log(text);
      
-    }
+   }
     
     
