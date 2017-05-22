5b9c12c4b2013875648e301535788f3ab85e67a3
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 13:44:05 +0300

    Update biggestLowestPrime.js

--- 3.HW_Js_Loops/1.Biggest_Lowest_Prime/biggestLowestPrime.js
+++ 3.HW_Js_Loops/1.Biggest_Lowest_Prime/biggestLowestPrime.js
@@ -1,14 +1,12 @@
 var  biggest;
-var	 lowest
+var   lowest
       b = prompt("Select interval length: From","");
       a = prompt("Select interval length: To","");
       
 
 for (biggest = a; biggest > 0;biggest--)
-if(biggest%2){
-break;}
-document.write("The biggest prime number is " + biggest +"<br>");
+      if(biggest%2){break;}
+      document.write("The biggest prime number is " + biggest +"<br>");
 for (lowest = b; lowest<100;lowest++)
-if(lowest%2){
-break;}
+      if(lowest%2){break;}
 document.write("The lowest prime number is " +lowest);
