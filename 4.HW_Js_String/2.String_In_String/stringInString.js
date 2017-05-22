4e30fd6b1e39829eb100336540d5ab7b3f7bfa66
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 13:35:42 +0300

    Update stringInString.js

--- 4.HW_Js_String/2.String_In_String/stringInString.js
+++ 4.HW_Js_String/2.String_In_String/stringInString.js
@@ -1,7 +1,5 @@
-function myFunction(){
-
-var firstOne = document.getElementById('firstString').value;
-var secondOne = document.getElementById('secondString').value;
-
-document.getElementById('space').innerHTML = (firstOne.split(secondOne).length-1);
+function submit(){
+  var firstOne = document.getElementById('firstString').value;
+  var secondOne = document.getElementById('secondString').value;
+  document.getElementById('space').innerHTML = (firstOne.split(secondOne).length-1);
 }
