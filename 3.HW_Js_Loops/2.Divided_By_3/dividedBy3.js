f4cd3594739cd74d2ee7ba0a0b17f1350ee90b7d
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 13:46:15 +0300

    Update dividedBy3.js

--- 3.HW_Js_Loops/2.Divided_By_3/dividedBy3.js
+++ 3.HW_Js_Loops/2.Divided_By_3/dividedBy3.js
@@ -2,11 +2,11 @@ num = 100;
 text = "";
 for(var i = 0; i <= num; i++)
     if(i % 3 == 0){
-        text += i + "<br>";
-        document.getElementById("space").innerHTML = text;
-console.log(i);}
+    text += i + "<br>";
+    document.getElementById("space").innerHTML = text;
+    console.log(i);}
 
     else{
- console.log("----")
- document.getElementById("space").innerHTML = text + "END";
+    console.log("----")
+    document.getElementById("space").innerHTML = text + "END";
     };
