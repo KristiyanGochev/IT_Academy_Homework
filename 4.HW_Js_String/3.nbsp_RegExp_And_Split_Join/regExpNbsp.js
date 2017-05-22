05738a14ebb597b11ecbf964c3b4436a91f0b3f2
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 13:38:46 +0300

    Update regExpNbsp.js

--- 4.HW_Js_String/3.nbsp_RegExp_And_Split_Join/regExpNbsp.js
+++ 4.HW_Js_String/3.nbsp_RegExp_And_Split_Join/regExpNbsp.js
@@ -1,9 +1,9 @@
 //RegExp Method
 
-function myFunction(){
+function submit(){
   var str = document.getElementById('space').innerHTML;
-var re = RegExp(" ", 'g');
-    var result = str.replace(re, "");
+  var re = RegExp(" ", 'g');
+  var result = str.replace(re, "");
   
   document.getElementById('space').innerHTML = result;
   document.getElementById("below").innerHTML = "You used RegExp method to accomplish this result" 
@@ -15,5 +15,5 @@ function regExp(){
     var text = document.getElementById('space').innerHTML;
     var nbsp = text.split(" ").join("");
     document.getElementById("space").innerHTML = nbsp;
-     document.getElementById("below").innerHTML = "You used the Split and Join methods to accomplish this result";
+    document.getElementById("below").innerHTML = "You used the Split and Join methods to accomplish this result";
 }
