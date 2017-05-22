e967a2bee75a614f4cfe6cef008669a5d614320a
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 13:37:11 +0300

    Update nbsp.js

--- 4.HW_Js_String/3.nbsp_RegExp_And_Split_Join/nbsp.js
+++ 4.HW_Js_String/3.nbsp_RegExp_And_Split_Join/nbsp.js
@@ -1,8 +1,8 @@
 //Split Join Method
 
-function myFunction(){
+function submit(){
     var text = document.getElementById('space').innerHTML;
     var nbsp = text.split(" ").join("");
     document.getElementById("space").innerHTML = nbsp;
-     document.getElementById("below").innerHTML = "You used the Split and Join methods to accomplish this result";
+    document.getElementById("below").innerHTML = "You used the Split and Join methods to accomplish this result";
 }
