f766cccfc04e840fc055b506dd644dd162c8fe0c
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 14:15:11 +0300

    Update Math.sqrt.js

--- 2.HW_Js_Conditional_Statements/3.Math.sqrt/Math.sqrt.js
+++ 2.HW_Js_Conditional_Statements/3.Math.sqrt/Math.sqrt.js
@@ -1,14 +1,13 @@
 var a = prompt("Enter value of a","");
-		var b = prompt("Enter value of b","");
-		var c = prompt("Enter value of c","");
+var b = prompt("Enter value of b","");
+var c = prompt("Enter value of c","");
  
-		var abc =(b * b - 4 * a * c);
-		var abc = -(abc);
-		var	root_part = Math.sqrt(abc);
-		var denom = 2 * a;
- 
-		var root1 = ( -b + root_part ) / denom;
-		var root2 = ( -b - root_part ) / denom;
- 
-		document.write("1st root: "+root1+"<br />");
-		document.write("2nd root: "+root2+"<br />");
+var abc =(b * b - 4 * a * c);
+var abc = -(abc);
+var	root_part = Math.sqrt(abc);
+var denom = 2 * a;
+var root1 = ( -b + root_part ) / denom;
+var root2 = ( -b - root_part ) / denom;
+
+document.write("1st root: "+root1+"<br />");
+document.write("2nd root: "+root2+"<br />");
