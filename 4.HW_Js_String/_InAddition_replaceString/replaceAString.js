7232bd086a01b3b417e3d07e6f14fd1f6a9759af
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-05-22 13:41:13 +0300

    Update replaceAString.js

--- 4.HW_Js_String/_InAddition_replaceString/replaceAString.js
+++ 4.HW_Js_String/_InAddition_replaceString/replaceAString.js
@@ -1,7 +1,7 @@
-function myFunction() {
+function submit() {
   var b = document.getElementById("second").value;
   var a = document.getElementById("first").value;
-    var str = document.getElementById("space").innerHTML; 
-    var txt = str.split(b).join(a);
-    document.getElementById("space").innerHTML = txt;
+  var str = document.getElementById("space").innerHTML; 
+  var txt = str.split(b).join(a);
+  document.getElementById("space").innerHTML = txt;
 }
