3164e8de8a8bde074e00600fee0f949ed73d4765
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-06-01 18:22:19 +0300

    Update querySelectorAll.js

--- 8.DOM/3.QuerySelectorAll/querySelectorAll.js
+++ 8.DOM/3.QuerySelectorAll/querySelectorAll.js
@@ -3,12 +3,14 @@ debugger;
 
   for (i = 0; i < x.length; i++) {
         if(i%2){
+          var op = document.createElement("div");
           var newP = document.createElement("p");
           var y = document.createTextNode(Date());
           
           newP.appendChild(y);
+          op.appendChild(newP);
            
-          x[i].appendChild(newP);
+          x[i].appendChild(op);
         }
         else{};
     }
