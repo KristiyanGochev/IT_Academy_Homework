7db71c2f286bc8c022f04f211d5bb371667b1251
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-06-05 09:31:05 +0300

    Update tryCatchValidation.js

--- 9.Events/3.TryCatchValidation/tryCatchValidation.js
+++ 9.Events/3.TryCatchValidation/tryCatchValidation.js
@@ -4,31 +4,17 @@
         var name = document.getElementById('firstName').value;
         var inpObj = document.getElementById("yourAge").value;
         try {
-            if (!isNaN(parseInt(name))) throw "Your name could not be a number!";
-            if (name == "") throw "First Name field is empty.";
-            if (inpObj == "") throw "The 'age' field is empty.";
-            if (inpObj == 0) throw "Your age could not be '0'";
-            if (inpObj < 0) throw "Your age could Not be negative";
-            if (isNaN(inpObj)) throw "not a number";
-            if (inpObj < 18) throw "You are unde age to vote";
-            if (inpObj >= 18) throw "Your Vote has been submited";
+            if (!isNaN(parseInt(name))) throw new Error("Your name could not be a number!");
+            if (name == "") throw new Error("First Name field is empty.");
+            if (inpObj == "") throw new Error("The 'age' field is empty.");
+            if (inpObj == 0) throw new Error("Your age could not be '0'");
+            if (inpObj < 0) throw new Error("Your age could Not be negative");
+            if (isNaN(inpObj)) throw new Error("not a number");
+            if (inpObj < 18) throw new Error("You are unde age to vote");
+            if (inpObj >= 18) throw new Error("Your Vote has been submited");
         }
         catch (err) {
             alert(err);
         }
     }
 }());
-
-
-
-
-// try {
-//     if (x == "") throw "empty";
-//     if (isNaN(x)) throw "not a number";
-//     x = Number(x);
-//     if (x < 5) throw "too low";
-//     if (x > 10) throw "too high";
-// }
-// catch (err) {
-//     alert(`Input is + ${err}`);
-// }
